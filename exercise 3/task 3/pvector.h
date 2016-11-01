d0459dcb92cf2e78e0fcdc82039b8cf2024a7bbd
Lukas Vollenweider <lukas.vollenweider@bluewin.ch>
2016-11-01 11:55:40 +0100

    no message

---
+++ exercise 3/task 3/pvector.h
@@ -0,0 +1,70 @@
+#ifndef PVECTOR_HPP_
+#define PVECTOR_HPP_
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "persistence_traits.h"
+
+template<typename T, typename P = persistence_traits<T>>
+class pvector {
+private:
+	typedef P persister;
+	typedef typename std::vector<T>::iterator iterator;
+	
+	std::string filename;
+	std::vector<T> v;
+
+	bool is_input_empty(std::ifstream& pFile) {
+	    return pFile.peek() == std::ifstream::traits_type::eof();
+	}
+public:
+	pvector(std::string fname) : filename(fname) { read_vector(); }
+	~pvector() { write_vector(); }
+
+	void push_back(const T &el) { v.push_back(el); }
+	void pop_back() { v.pop_back(); }
+	T back() { return v.back(); }
+	T operator[] (std::size_t pos) { return v.at(pos); }
+	T at(int i){ return v.at(i); }
+	std::size_t size() { return v.size(); }
+	iterator begin() { return v.begin(); }
+	iterator end() { return v.end(); }
+
+	void print_vector(){
+		typename std::vector<T>::iterator fst = v.begin(), lst = v.end();
+		while(fst != lst) {
+			std::cout << *fst++ << std::endl;
+		}
+	}
+
+	void read_vector() {
+		std::ifstream ifs(filename);
+		if(!is_input_empty(ifs)) {
+			while(true) {
+				if(!ifs.good()) { break; }
+				T x; 
+				persister::read(ifs, x);
+				v.push_back(x);
+			}
+		}
+		ifs.close();
+	}
+
+	void write_vector() {
+		std::ofstream ofs (filename.c_str(), std::ofstream::trunc);
+		typename std::vector<T>::iterator fst = v.begin(), lst = v.end();
+		while (fst != lst) {
+			persister::write(ofs, *fst++);
+			//Since we advanced the fst iterator, we can check if we have
+			//any element left. If we do, we print a new line.
+			if (fst != lst) { 
+				ofs << std::endl; 
+			}
+		}
+		ofs.close();
+	}
+};
+
+#endif /* PVECTOR_HPP_ */
