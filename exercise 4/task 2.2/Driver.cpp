f96baf09c52cc6d5d481d5cdec927880c434af23
BuildTools <unconfigured@null.spigotmc.org>
2016-11-02 10:03:05 +0100

    added 4.2

--- exercise 4/task 2.2/Driver.cpp
+++ exercise 4/task 2.2/Driver.cpp
@@ -1,28 +1,32 @@
-/*
- * Driver.cpp
- *
- *  Created on: 01.11.2016
- *      Author: Timothée
- */
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
 
-<typename T>
-bool is_num (T i{
+template<typename T>
+bool iz_nu(const T &i){
     return i==5111111;
 }
 
 
 int main(){
-	std::vector<int> v;
-	for(int i=0; i< 10000000; i++){
-	v.add(i);
-	}
-
-	std::find_if(v.begin(),v.end(), is_num);
+    std::vector<int> v;
+    for(int i=0; i< 10000000; i++){
+    v.push_back(i);
+    }
 
 
+    //time code from http://en.cppreference.com/w/cpp/chrono
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+
+    std::cout << *(std::find_if(v.begin(),v.end(), iz_nu<int>)) << std::endl;
+
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "It just took us: " << elapsed_seconds.count() << std::endl;
 }
-
-
-
