4a8800bf41a3d69b9d6e0eb7dd27bf404a75ebbc
BuildTools <unconfigured@null.spigotmc.org>
2017-01-11 11:23:16 +0100

    added nico player

--- exercise 8/task 1/driver.cpp
+++ exercise 8/task 1/driver.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-	player *p1 = playerFactory::make("H");
+	player *p1 = playerFactory::make("N(1)");
 	player *p2 = playerFactory::make("T");
 	connect4 con(p1, p2);
 	return 0;
---
+++ exercise 8/task 1/player_nico.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER_NICO__H
+#define PLAYER_NICO__H
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <iostream> 
+#include "player.hpp"
+
+class player_nico : public player {
+public:
+	int player_id;
+ 	player_nico(int id) : player_id(id){ srand (time(NULL));}
+	virtual ~player_nico() {}
+ 	virtual int play(const playfield &field);
+ 	static player *make(const char*);
+private:
+	int check(int x, int y, int player, int offset_x, int offset_y, const playfield &field);
+ 	int getFreeHeight(int col, const playfield &field);
+};
+#endif
